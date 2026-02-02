f22128be79cc7c749571174d9156124cc4a0b901
AuraJosh <josh.witte.business@gmail.com>
2026-02-02 01:36:23 +0000

    New folders

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './', // Critical for GitHub Pages
-    resolve: {
-        alias: {
-            '@': '/src',
-        },
+    base: "/car-match-advisor/",
+    // ADD THIS SECTION:
+    build: {
+        outDir: 'docs',
     },
-});
+})
